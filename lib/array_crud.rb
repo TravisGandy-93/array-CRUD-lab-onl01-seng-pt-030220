78530f8f9c9f6f1918a15b94570a14907b389e50
Travis Gandy <travis_gandy@yahoo.com>
2020-03-08 23:48:34 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -20,9 +20,9 @@ end
 
 def remove_element_from_end_of_array(array)
   array =  ["F","T","W","G"]
-  array.pop
+  bye = array.pop
 end
-
+puts bye
 
 def remove_element_from_start_of_array(array)
   
