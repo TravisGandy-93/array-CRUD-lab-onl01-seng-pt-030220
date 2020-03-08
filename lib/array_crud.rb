c36376a60f0d0cbe73d486f12a89fa0cc4f5cb62
Travis Gandy <travis_gandy@yahoo.com>
2020-03-08 23:39:23 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,13 +1,16 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+  ["F","T","W","G"]
 end
 
+
+array =  ["F","T","W","G"]
 def add_element_to_end_of_array(array, element)
-  
+  element = "A"
+  array << element
 end
 
 def add_element_to_start_of_array(array, element)
